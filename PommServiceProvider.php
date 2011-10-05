6ee7fd7fe4a246eb6a0bfc9feac81bcf623f23c8
Grégoire HUBERT <gregoire.hubert.pro@gmail.com>
2011-10-05 18:03:07 +0200

    first service provider from the old Extension

---
+++ PommServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GHub\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class PommServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        if (isset($app['pomm.class_path'])) {
+            $app['autoloader']->registerNamespace('Pomm', $app['pomm.class_path']);
+        }
+
+        $app['pomm'] = $app->share(function() use ($app) {
+            $class_name = isset($app['pomm.class_name']) ? $app['pomm.class_name'] : 'Pomm\Service';
+            $service = new $class_name($app['pomm.connections']);
+
+            return $service;
+        });
+    }
+}
