985fc8058a8040093f048912dcd9ccdce6d3304e
Jérôme Tamarelle <jerome@tamarelle.net>
2012-10-09 09:09:55 +0200

    Remove autoloading as it is no longer available in Silex (replaced by composer)

--- src/Pomm/Silex/PommServiceProvider.php
+++ src/Pomm/Silex/PommServiceProvider.php
@@ -9,13 +9,9 @@ class PommServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        if (isset($app['pomm.class_path'])) {
-            $app['autoloader']->registerNamespace('Pomm', $app['pomm.class_path']);
-        }
-
+        $app['pomm.class_name'] = 'Pomm\Service';
         $app['pomm'] = $app->share(function() use ($app) {
-            $class_name = isset($app['pomm.class_name']) ? $app['pomm.class_name'] : 'Pomm\Service';
-            $service = new $class_name($app['pomm.databases']);
+            $service = new $app['pomm.class_name']($app['pomm.databases']);
 
             return $service;
         });
